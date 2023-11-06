65aaa9ae50c16787ed5fbde9622b00459dc2e787
Medinatu <medinatuali@gmail.com>
2023-11-06 11:56:34 -0800

    0x03_python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,71 +1,102 @@
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
+#include <stdlib.h>
+
+#include <stdio.h>
+
 
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
+
+*add_nodeint - adds a new node at the beginning of a listint_t list
+
+*@head: head of listint_t
+
+*@n: int to add in listint_t list
+
+*Return: address of the new element, or NULL if it failed
+
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+
 {
-	listint_t *node = *head, *next, *prev = NULL;
 
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
+        listint_t *new;
 
-	*head = prev;
-	return (*head);
+
+        new = malloc(sizeof(listint_t));
+
+        if (new == NULL)
+
+                return (NULL);
+
+        new->n = n;
+
+        new->next = *head;
+
+        *head = new;
+
+        return (new);
+
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
+
+*is_palindrome - identify if a syngle linked list is palindrome
+
+*@head: head of listint_t
+
+*Return: 1 if it is palindrome else 0
+
+*/
+
 int is_palindrome(listint_t **head)
+
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+        listint_t *head2 = *head;
 
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
+        listint_t *aux = NULL, *aux2 = NULL;
 
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
 
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
+        if (*head == NULL || head2->next == NULL)
 
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
+                return (1);
 
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
+        while (head2 != NULL)
 
-	return (1);
+        {
+
+                add_nodeint(&aux, head2->n);
+
+                head2 = head2->next;
+
+        }
+
+        aux2 = aux;
+
+        while (*head != NULL)
+
+        {
+
+                if ((*head)->n != aux2->n)
+
+                {
+
+                        free_listint(aux);
+
+                        return (0);
+
+                }
+
+                *head = (*head)->next;
+
+                aux2 = aux2->next;
+
+        }
+
+        free_listint(aux);
+
+        return (1);
+
+}
